d32f57bf23b24f71a0268aaa6b17797157080a09
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-11-27 11:37:04 +0800

    用  JSON 替换 nlohmann json

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -12,23 +12,16 @@ int main(int argc, char** argv) {
   std::string path = local.parent_path().append("modules/core/localization.json").string();
 
   if (std::filesystem::path(path).filename() == std::string("localization.json")) {
-    std::ifstream ifs(path);
-    if (ifs.is_open()) {
-      nlohmann::json data = nlohmann::json::parse(ifs);
-
-      for (const auto& item : data.items()) { 
-        if (item.value().is_object()) {
-          for (const auto& _item : item.value().items()) { 
-            std::cout << "Key: " << _item.key() << ", Value: " << _item.value() << std::endl;  
-          }
-        } else {
-          std::cout << "Key: " << item.key() << ", Value: " << item.value() << std::endl;  
+    Localization localization(path);
+    localization.load();
+    for (const auto& item : localization.items()) {
+      if (item.value().is_object()) {
+        for (const auto& _item : item.value().items()) { 
+          std::cout << "Key: " << _item.key() << ", Value: " << _item.value() << std::endl;  
         }
-      }  
-      
-      ifs.close();
-    } else {
-      Err("文件打开失败: %s", path.c_str());
+      } else {
+        std::cout << "Key: " << item.key() << ", Value: " << item.value() << std::endl;  
+      }
     }
   }
 
